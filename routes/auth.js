520549f8cf4d803529ba2646ae0bb2f46a2ed9d0
tejj1221 <tejj1221@gmail.com>
2024-07-02 12:58:30 +0530

    updated vercel code

--- routes/auth.js
+++ routes/auth.js
@@ -3,36 +3,42 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-
+require('dotenv').config(); 
 // Register a new user
 router.post('/register', async (req, res) => {
     const { username, password } = req.body;
 
     try {
+        // Check if the user already exists
         let user = await User.findOne({ username });
         if (user) {
             return res.status(400).json({ msg: 'User already exists' });
         }
 
+        // Create a new user instance
         user = new User({
             username,
             password,
         });
 
+        // Hash the password
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password, salt);
 
+        // Save the user to the database
         await user.save();
 
+        // Generate JWT token
         const payload = {
             user: {
                 id: user.id,
             },
         };
 
+        // Sign the token
         jwt.sign(
             payload,
-            'yourJWTSecret',
+            process.env.JWT_SECRET, // Use environment variable for JWT secret
             { expiresIn: 3600 },
             (err, token) => {
                 if (err) throw err;
@@ -50,25 +56,29 @@ router.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
     try {
+        // Check if the user exists
         let user = await User.findOne({ username });
         if (!user) {
             return res.status(400).json({ msg: 'Invalid Credentials' });
         }
 
+        // Check password
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(400).json({ msg: 'Invalid Credentials' });
         }
 
+        // Generate JWT token
         const payload = {
             user: {
                 id: user.id,
             },
         };
 
+        // Sign the token
         jwt.sign(
             payload,
-            'yourJWTSecret',
+            process.env.JWT_SECRET, // Use environment variable for JWT secret
             { expiresIn: 3600 },
             (err, token) => {
                 if (err) throw err;
